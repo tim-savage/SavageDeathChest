a988afe7b4ef0d0674bedf336256795f17e515d3
Tim Savage <tim.savage@gmail.com>
2018-10-30 01:26:39 -0500

    added additional values and field

--- SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/SearchResult.java
+++ SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/SearchResult.java
@@ -1,25 +1,32 @@
 package com.winterhaven_mc.deathchest;
 
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public enum SearchResult {
 
 	SUCCESS,
+	PARTIAL_SUCCCESS,
 	PROTECTION_PLUGIN,
 	NON_REPLACEABLE_BLOCK,
 	ABOVE_GRASS_PATH,
-	ADJACENT_CHEST;
+	ADJACENT_CHEST,
+	NO_CHEST;
 	
 
 	private Location location;
 	private ProtectionPlugin protectionPlugin;
+	private List<ItemStack> remainingItems = new ArrayList<>();
 
 	public Location getLocation() {
 		return location;
 	}
 
 	public void setLocation(final Location location) {
-		this.location = location;
+		this.location = location.clone();
 	}
 
 	public ProtectionPlugin getProtectionPlugin() {
@@ -29,5 +36,9 @@ public enum SearchResult {
 	public void setProtectionPlugin(final ProtectionPlugin protectionPlugin) {
 		this.protectionPlugin = protectionPlugin;
 	}
-	
+
+	public List<ItemStack> getRemainingItems() { return remainingItems; }
+
+	public void setRemainingItems(List<ItemStack> remainingItems) { this.remainingItems = remainingItems; }
+
 }
