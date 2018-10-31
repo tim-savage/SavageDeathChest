cfa6c0495df2e1b7d7e2a471fb58bf4366d31d12
Tim Savage <tim.savage@gmail.com>
2018-10-31 05:39:14 -0500

    initial commit

---
+++ SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/storage/DeathRecord.java
@@ -0,0 +1,91 @@
+package com.winterhaven_mc.deathchest.storage;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+
+public class DeathRecord {
+
+    // the chest location for this death record
+    private Location location;
+
+    // the UUID of the owner of this death record
+    private UUID ownerUUID;
+
+    // the UUID of the player who killed the death record owner, if any; otherwise null
+    private UUID killerUUID;
+
+    // the expiration time of this death record, in milliseconds since epoch
+    private long expiration;
+
+    // number of items removed from this death record by killer
+    private int itemsRemoved;
+
+
+    /**
+     * Empty DeathChest constructor
+     */
+    public DeathRecord() {}
+
+
+    /**
+     * DeathChest constructor
+     */
+    public DeathRecord(Player player) {
+
+        this.ownerUUID = player.getUniqueId();
+    }
+
+
+    /**
+     * DeathChest constructor
+     */
+    public DeathRecord(Player player, Location location) {
+
+        this.ownerUUID = player.getUniqueId();
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public UUID getOwnerUUID() {
+        return ownerUUID;
+    }
+
+    public void setOwnerUUID(UUID ownerUUID) {
+        this.ownerUUID = ownerUUID;
+    }
+
+    public UUID getKillerUUID() {
+        return killerUUID;
+    }
+
+    public void setKillerUUID(UUID killerUUID) {
+        this.killerUUID = killerUUID;
+    }
+
+    public long getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(long expiration) {
+        this.expiration = expiration;
+    }
+
+    public int getItemsRemoved() {
+        return itemsRemoved;
+    }
+
+    public void setItemsRemoved(int itemsRemoved) {
+        this.itemsRemoved = itemsRemoved;
+    }
+
+}
