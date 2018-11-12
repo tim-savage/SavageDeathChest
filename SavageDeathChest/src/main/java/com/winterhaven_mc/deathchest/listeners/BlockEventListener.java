b5771326eaaa5fcf0d6fca829b82d6d1909830d1
Tim Savage <tim.savage@gmail.com>
2018-11-12 03:15:57 -0600

    refactor

--- SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/listeners/BlockEventListener.java
+++ SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/listeners/BlockEventListener.java
@@ -1,10 +1,11 @@
 package com.winterhaven_mc.deathchest.listeners;
 
-import com.winterhaven_mc.deathchest.chests.DeathChestBlock;
 import com.winterhaven_mc.deathchest.PluginMain;
-import com.winterhaven_mc.deathchest.ProtectionPlugin;
+//import com.winterhaven_mc.deathchest.ProtectionPlugin;
+import com.winterhaven_mc.deathchest.chests.DeathChest;
 import com.winterhaven_mc.deathchest.messages.MessageId;
 import com.winterhaven_mc.deathchest.sounds.SoundId;
+
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Location;
@@ -21,7 +22,6 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 
 import java.util.ArrayList;
 
-import static com.winterhaven_mc.deathchest.chests.DeathChestBlock.isDeathChest;
 import static com.winterhaven_mc.deathchest.util.LocationUtilities.*;
 
 
@@ -56,23 +56,23 @@ public final class BlockEventListener implements Listener {
 		final Block block = event.getBlock();
 		final Location location = block.getLocation();
 
-
 		// if placed block is not a chest, do nothing and return
 		if (!block.getType().equals(Material.CHEST)) {
 			return;
 		}
 
 		// check for adjacent death chests
-		if (isDeathChest(getBlockToLeft(location))
-				|| isDeathChest(getBlockToRight(location))
-				|| isDeathChest(getBlockToFront(location))
-				|| isDeathChest(getBlockToRear(location))) {
+		if (plugin.chestManager.isDeathChestChestBlock(getBlockToLeft(location))
+				|| plugin.chestManager.isDeathChestChestBlock(getBlockToRight(location))
+				|| plugin.chestManager.isDeathChestChestBlock(getBlockToFront(location))
+				|| plugin.chestManager.isDeathChestChestBlock(getBlockToRear(location))) {
 			if (plugin.debug) {
-				plugin.getLogger().info("Chest placed adjacent death chest cancelled.");
+				plugin.getLogger().info("Chest placed adjacent to death chest cancelled.");
 			}
 			event.setCancelled(true);
 		}
 	}
+
 
 	/**
 	 * Block break event handler<br>
@@ -82,26 +82,44 @@ public final class BlockEventListener implements Listener {
 	@EventHandler(priority = EventPriority.LOW)
 	//TODO: Make sure we're using the right priority. Use NORMAL if possible.
 	public final void onBlockBreak(final BlockBreakEvent event) {
-		
+
+		if (plugin.debug) {
+			plugin.getLogger().info("Caught BlockBreakEvent.");
+		}
+
 		final Block block = event.getBlock();
 		final Player player = event.getPlayer();
-		
-		// get instance of DeathChestBlock from event block
-		final DeathChestBlock deathChestBlock = DeathChestBlock.getChestInstance(block);
 
 		// if event block is not a DeathChestBlock, do nothing and return
-		if (deathChestBlock == null) {
+		if (!plugin.chestManager.isDeathChestComponent(block)) {
+			return;
+		}
+
+		// if block is DeathChest sign, set drop items false
+		if (plugin.chestManager.isDeathChestSignBlock(block)) {
+			if (plugin.debug) {
+				plugin.getLogger().info("Caught BlockBreakEvent for DeathChest sign.");
+			}
+			block.getDrops().clear();
+			return;
+		}
+
+		// get instance of DeathChest from event block
+		final DeathChest deathChest = plugin.chestManager.getDeathChest(block);
+
+		// if event block is not a DeathChestBlock, do nothing and return
+		if (deathChest == null) {
 			return;
 		}
 		
 		// if access is blocked by a protection plugin, do nothing and return (allow protection plugin to handle event)
-		ProtectionPlugin blockingPlugin = ProtectionPlugin.allowChestAccess(player, block);
-		if (blockingPlugin != null) {
-			if (plugin.debug) {
-				plugin.getLogger().info(blockingPlugin.getPluginName() + " prevented access to a chest.");
-			}
-			return;
-		}
+//		ProtectionPlugin blockingPlugin = ProtectionPlugin.allowChestAccess(player, block);
+//		if (blockingPlugin != null) {
+//			if (plugin.debug) {
+//				plugin.getLogger().info(blockingPlugin.getPluginName() + " prevented access to a chest.");
+//			}
+//			return;
+//		}
 		
 		// if player is in creative mode 
 		// and creative-access is configured false
@@ -124,7 +142,7 @@ public final class BlockEventListener implements Listener {
 		event.setCancelled(true);
 		
 		// if chest is already open, disallow breakage; send message and return
-		if (deathChestBlock.getViewerCount() > 0) {
+		if (deathChest.getViewerCount() > 0) {
 
 			// send player message
 			plugin.messageManager.sendMessage(player, MessageId.CHEST_CURRENTLY_OPEN);
@@ -135,16 +153,16 @@ public final class BlockEventListener implements Listener {
 		}
 
 		// if player is owner or has deathchest.loot.other permission, break chest and return
-		if (deathChestBlock.isOwner(player) || player.hasPermission("deathchest.loot.other")) {
-			deathChestBlock.destroy();
+		if (deathChest.isOwner(player) || player.hasPermission("deathchest.loot.other")) {
+			deathChest.destroy();
 			return;
 		}
 
 		// if killer looting is enabled and player is killer, break chest and return
 		// TODO: this will need to be removed when items taken limit is implemented
 		if (plugin.getConfig().getBoolean("killer-looting") 
-				&& deathChestBlock.isKiller(player)) {
-			deathChestBlock.destroy();
+				&& deathChest.isKiller(player)) {
+			deathChest.destroy();
 			return;
 		}
 
@@ -169,10 +187,10 @@ public final class BlockEventListener implements Listener {
 			return;
 		}
 		
-		// iterate through all blocks in explosion event and remove those that are DeathChestBlocks
+		// iterate through all blocks in explosion event and remove those that are DeathChest chests or signs
 		ArrayList<Block> blocks = new ArrayList<>(event.blockList());
 		for (Block block : blocks) {
-			if (DeathChestBlock.isDeathChestBlock(block)) {
+			if (plugin.chestManager.isDeathChestComponent(block)) {
 				event.blockList().remove(block);
 			}
 		}
@@ -192,16 +210,16 @@ public final class BlockEventListener implements Listener {
 			return;
 		}
 		
-		// iterate through all blocks in explosion event and remove those that are DeathChestBlocks
+		// iterate through all blocks in explosion event and remove those that are DeathChest chests or signs
 		ArrayList<Block> blocks = new ArrayList<>(event.blockList());
 		for (Block block : blocks) {
-			if (DeathChestBlock.isDeathChestBlock(block)) {
+			if (plugin.chestManager.isDeathChestComponent(block)) {
 				event.blockList().remove(block);
 			}
 		}
 	}
 
-	
+
 	/**
 	 * Block physics event handler<br>
 	 * remove detached death chest signs from game to prevent players gaining additional signs
@@ -209,33 +227,11 @@ public final class BlockEventListener implements Listener {
 	 */
 	@EventHandler
 	public final void signDetachCheck(final BlockPhysicsEvent event) {
-	
-		// if event is already cancelled, do nothing and return
-		if (event.isCancelled()) {
-			return;
-		}
 
-		// get instance of DeathChestBlock representing the event block
-		final DeathChestBlock deathSign = DeathChestBlock.getSignInstance(event.getBlock());
-
-		// if block is not a death chest sign, do nothing and return
-		if (deathSign == null) {
-			return;
+		// if event block is a DeathChest component, cancel event
+		if (plugin.chestManager.isDeathChestSignBlock(event.getBlock())) {
+			event.setCancelled(true);
 		}
-		
-		// get block that sign is attached to
-		final Block attachedBlock = deathSign.getAttachedBlock();
-	    
-		// if attached block is still there, do nothing and return
-		if (attachedBlock != null && attachedBlock.getType() != Material.AIR) {
-			return;
-		}
-		
-		// cancel event
-		event.setCancelled(true);
-		
-		// destroy the death sign
-		deathSign.destroy();
 	}
 
 }
