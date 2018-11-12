4c31067faa62feb38a15057948d381cf4ff5cc8c
Tim Savage <tim.savage@gmail.com>
2018-11-12 02:59:50 -0600

    move replaceableBlocks into chestManager

--- SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/PluginMain.java
+++ SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/PluginMain.java
@@ -1,10 +1,9 @@
 package com.winterhaven_mc.deathchest;
 
-import com.winterhaven_mc.deathchest.chests.ReplaceableBlocks;
+import com.winterhaven_mc.deathchest.chests.ChestManager;
 import com.winterhaven_mc.deathchest.listeners.BlockEventListener;
 import com.winterhaven_mc.deathchest.listeners.InventoryEventListener;
 import com.winterhaven_mc.deathchest.listeners.PlayerEventListener;
-import com.winterhaven_mc.deathchest.chests.ChestManager;
 import com.winterhaven_mc.deathchest.storage.DataStore;
 import com.winterhaven_mc.deathchest.storage.DataStoreFactory;
 import com.winterhaven_mc.deathchest.commands.CommandManager;
@@ -25,7 +24,6 @@ public final class PluginMain extends JavaPlugin {
 	public SoundConfiguration soundConfig;
 	public DataStore dataStore;
 	public ChestManager chestManager;
-	public ReplaceableBlocks replaceableBlocks;
 
 	public boolean debug = getConfig().getBoolean("debug");
 
@@ -49,9 +47,6 @@ public final class PluginMain extends JavaPlugin {
 		// instantiate datastore
 		dataStore = DataStoreFactory.create();
 
-		// instantiate replaceable blocks
-		replaceableBlocks = new ReplaceableBlocks(this);
-
 		// instantiate chest manager
 		chestManager = new ChestManager(this);
 
@@ -65,7 +60,6 @@ public final class PluginMain extends JavaPlugin {
 
 		// log detected protection plugins
 		ProtectionPlugin.reportInstalled();
-
 	}
 
 	public void onDisable() {
