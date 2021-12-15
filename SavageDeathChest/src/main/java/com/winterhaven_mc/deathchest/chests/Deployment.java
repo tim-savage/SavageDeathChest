61971bb8d9f06e26b4e2229f6f2228c4a3cfe7f9
Tim Savage <tim.savage@gmail.com>
2021-12-15 15:34:44 -0600

    revert to java 8 compatible unmodifiable sets

--- SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/chests/Deployment.java
+++ SavageDeathChest/src/main/java/com/winterhaven_mc/deathchest/chests/Deployment.java
@@ -40,11 +40,11 @@ public final class Deployment {
 	private final DeathChest deathChest;
 
 	// set of path block type names as strings
-	private static final Set<String> pathBlockTypeNames = Set.of(
+	private static final Set<String> pathBlockTypeNames = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
 			"GRASS_PATH",
 			"LEGACY_GRASS_PATH",
 			"DIRT_PATH"
-	);
+	)));
 
 
 	/**
