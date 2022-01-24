7699092e145aeacc47efec0bcfaf4d42fda72e75
Tim Savage <tim.savage@gmail.com>
2022-01-23 22:39:48 -0600

    add null check for area; add javadocs

--- src/main/java/com/winterhavenmc/deathchest/protectionchecks/plugins/Lands.java
+++ src/main/java/com/winterhavenmc/deathchest/protectionchecks/plugins/Lands.java
@@ -5,15 +5,29 @@ import com.winterhavenmc.deathchest.protectionchecks.ProtectionPluginAbstract;
 import me.angeschossen.lands.api.integration.LandsIntegration;
 import me.angeschossen.lands.api.land.Area;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+import javax.annotation.Nullable;
 
+
+/**
+ * Class that implements protection plugin checks for Lands
+ */
 public final class Lands extends ProtectionPluginAbstract implements ProtectionPlugin {
 
+	// reference to LandsIntegration instance
 	LandsIntegration landsIntegration;
 
+
+	/**
+	 * Class constructor
+	 *
+	 * @param plugin reference to SavageDeathChest plugin main class instance
+	 * @param name name of the protection plugin
+	 * @param version version of the protection plugin
+	 */
 	public Lands(final JavaPlugin plugin, final String name, final String version) {
 		this.plugin = plugin;
 		this.name = name;
@@ -27,9 +41,11 @@ public final class Lands extends ProtectionPluginAbstract implements ProtectionP
 	public boolean allowChestPlacement(final Player player, final Location location) {
 
 		try {
-			final Area area = landsIntegration.getAreaByLoc(location);
-			// this query returns true if placement is allowed, false if placement is denied NOTE: unconfirmed for pay plugin
-			return (Objects.requireNonNull(area).hasFlag(player, me.angeschossen.lands.api.flags.Flags.BLOCK_PLACE, false));
+			final @Nullable Area area = landsIntegration.getAreaByLoc(location);
+			if (area != null) {
+				// this query should return true if placement is allowed, false if placement is denied NOTE: unconfirmed for paid plugin
+				return area.hasFlag(player, me.angeschossen.lands.api.flags.Flags.BLOCK_PLACE, Material.CHEST, false);
+			}
 		}
 		catch (Exception e) {
 			logPlaceError();
@@ -42,9 +58,11 @@ public final class Lands extends ProtectionPluginAbstract implements ProtectionP
 	public boolean allowChestAccess(final Player player, final Location location) {
 
 		try {
-			final Area area = landsIntegration.getAreaByLoc(location);
-			// this query return true if access is allowed, false if access is denied NOTE: unconfirmed for pay plugin
-			return Objects.requireNonNull(area).hasFlag(player, me.angeschossen.lands.api.flags.Flags.INTERACT_CONTAINER, false);
+			final @Nullable Area area = landsIntegration.getAreaByLoc(location);
+			if (area != null) {
+				// this query should return true if access is allowed, false if access is denied NOTE: unconfirmed for paid plugin
+				return area.hasFlag(player, me.angeschossen.lands.api.flags.Flags.INTERACT_CONTAINER, false);
+			}
 		}
 		catch (Exception e) {
 			logAccessError();
